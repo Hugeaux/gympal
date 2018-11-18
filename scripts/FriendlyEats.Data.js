8eb11f2abfb6f846416bcd53b6fbd62c133ec007
Hugo <hugovndrhorst@gmail.com>
2018-11-18 09:54:59 +0000

    first push

---
+++ scripts/FriendlyEats.Data.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+FriendlyEats.prototype.getGym = function(id) {
+  return firebase.firestore().collection('gyms').doc(id).get();
+};
+
+FriendlyEats.prototype.getAllGyms = function(renderer) {
+   var query = firebase.firestore()
+      .collection('gyms')
+      .orderBy('name', 'asc')
+      .limit(50);
+
+
+
+FriendlyEats.prototype.getDocumentsInQuery = function(query, renderer) {
+   query.onSnapshot(function(snapshot) {
+    if (!snapshot.size) return renderer.empty(); // Display "There are no gyms".
+
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        renderer.remove(change.doc);
+      } else {
+        renderer.display(change.doc);
+      }
+    });
+  });
+};
+
+FriendlyEats.prototype.getGym = function(id) {
+  return firebase.firestore().collection('gyms').doc(id).get();
+};
+
+  this.getDocumentsInQuery(query, renderer);
+};
+
+FriendlyEats.prototype.getFilteredGyms = function(filters, renderer) {
+    var query = firebase.firestore().collection('gyms');
+
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price.length);
+  }
+
+   if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRatings', 'desc');
+  }
+
+  this.getDocumentsInQuery(query, renderer);
+};
+
+FriendlyEats.prototype.addRating = function(gymsID, rating) {
+  var collection = firebase.firestore().collection('gyms');
+  var document = collection.doc(gymsID);
+  var newRatingDocument = document.collection('ratings').doc();
+
+  return firebase.firestore().runTransaction(function(transaction) {
+    return transaction.get(document).then(function(doc) {
+      var data = doc.data();
+
+      var newAverage =
+          (data.numRatings * data.avgRating + rating.rating) /
+          (data.numRatings + 1);
+
+      transaction.update(document, {
+        numRatings: data.numRatings + 1,
+        avgRating: newAverage
+      });
+      return transaction.set(newRatingDocument, rating);
+    });
+  });
+};
---
+++ scripts/FriendlyEats.js
@@ -0,0 +1,146 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+/**
+ * Initializes the FriendlyEats app.
+ */
+function FriendlyEats() {
+  this.filters = {
+    city: '',
+    price: '',
+    category: '',
+    sort: 'Rating'
+  };
+
+  this.dialogs = {};
+
+  firebase.firestore().settings({ timestampsInSnapshots: true });
+
+  var that = this;
+  firebase.auth().signInAnonymously().then(function() {
+    that.initTemplates();
+    that.initRouter();
+    that.initReviewDialog();
+    that.initFilterDialog();
+  }).catch(function(err) {
+    console.log(err);
+  });
+}
+
+/**
+ * Initializes the router for the FriendlyEats app.
+ */
+FriendlyEats.prototype.initRouter = function() {
+  this.router = new Navigo();
+
+  var that = this;
+  this.router
+    .on({
+      '/': function() {
+        that.updateQuery(that.filters);
+      }
+    })
+    .on({
+      '/setup': function() {
+        that.viewSetup();
+      }
+    })
+    .on({
+      '/gyms/*': function() {
+        var path = that.getCleanPath(document.location.pathname);
+        var id = path.split('/')[2];
+        that.viewGym(id);
+      }
+    })
+    .resolve();
+
+  firebase
+    .firestore()
+    .collection('gyms')
+    .limit(1)
+    .onSnapshot(function(snapshot) {
+      if (snapshot.empty) {
+        that.router.navigate('/setup');
+      }
+    });
+};
+
+FriendlyEats.prototype.getCleanPath = function(dirtyPath) {
+  if (dirtyPath.startsWith('/index.html')) {
+    return dirtyPath.split('/').slice(1).join('/');
+  } else {
+    return dirtyPath;
+  }
+};
+
+FriendlyEats.prototype.getFirebaseConfig = function() {
+  return firebase.app().options;
+};
+
+FriendlyEats.prototype.getRandomItem = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+FriendlyEats.prototype.data = {
+  words: [
+    'Bar',
+    'Fire',
+    'Grill',
+    'Drive Thru',
+    'Place',
+    'Best',
+    'Spot',
+    'Prime',
+    'Eatin\''
+  ],
+  cities: [
+    'Amsterdam',
+    'Rotterdam',
+    'The Hague',
+  ],
+  categories: [
+    'Powerlifting',
+    'Bodybuilding',
+    'Cardio'
+  ],
+  ratings: [
+    {
+      rating: 1,
+      text: 'Would never eat here again!'
+    },
+    {
+      rating: 2,
+      text: 'Not my cup of tea.'
+    },
+    {
+      rating: 3,
+      text: 'Exactly okay :/'
+    },
+    {
+      rating: 4,
+      text: 'Actually pretty good, would recommend!'
+    },
+    {
+      rating: 5,
+      text: 'This is my favorite place. Literally.'
+    }
+  ]
+};
+
+window.onload = function() {
+  window.app = new FriendlyEats();
+};
